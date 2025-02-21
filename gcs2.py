2ee3862f2a602e59170c031de51dc3a1423013a9
Cameron Taggart <cataggar@microsoft.com>
2025-02-21 11:20:50 +0000

    relative path in gcs2

--- gcs2.py
+++ gcs2.py
@@ -4,11 +4,14 @@ from mitmproxy.tools.main import mitmproxy
 # https://docs.mitmproxy.org/stable/concepts-options/
 
 os.environ['SSLKEYLOGFILE'] = os.path.expanduser('~/certs/key.log')
+cert = os.path.expanduser('~/certs/gcs.ppe.monitoring.core.windows.net.pem')
 
 args = ["--set", "http2=false",
-        "--set", "client_certs=/Users/cataggar/certs/gcs.ppe.monitoring.core.windows.net.pem",
-        # "--set", "tls_version_client_min=TLS1_2",
-        # "--set", "tls_version_server_min=TLS1_2",
+        "--set", f"client_certs={cert}",
+        "--set", "tls_version_client_min=TLS1_2",
+        "--set", "tls_version_server_min=TLS1_2",
+        "--set", "tls_version_client_max=TLS1_2",
+        "--set", "tls_version_server_max=TLS1_2",
         # "--set", "proxy_debug=true",
         # "--set", "console_eventlog_verbosity=debug",
         ]
