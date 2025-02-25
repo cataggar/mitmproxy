d50b761986e48d7062f2708cc8017f7ea8cb4afe
Cameron Taggart <cataggar@microsoft.com>
2025-02-25 14:39:42 +0000

    gcs6 working without tls max https://github.com/mitmproxy/mitmproxy/pull/7576

--- gcs6.py
+++ gcs6.py
@@ -1,18 +1,13 @@
 import os
 from mitmproxy.tools.main import mitmproxy
 
-# https://docs.mitmproxy.org/stable/concepts-options/
-
 os.environ['SSLKEYLOGFILE'] = os.path.expanduser('~/certs/key.log')
 cert = os.path.expanduser('~/certs/gcs.ppe.monitoring.core.windows.net.pem')
 
-args = ["--set", "http2=false",
+# https://docs.mitmproxy.org/stable/concepts-options/
+args = [
+        "--set", "http2=false",
         "--set", f"client_certs={cert}",
-        # "--set", "tls_version_client_min=TLS1_2",
-        # "--set", "tls_version_server_min=TLS1_2",
-        # "--set", "tls_version_client_max=TLS1_2",
-        "--set", "tls_version_server_max=TLS1_2",
-        # "--set", "proxy_debug=true",
-        # "--set", "console_eventlog_verbosity=debug",
+        # "--set", "tls_version_server_max=TLS1_2",
         ]
 mitmproxy(args)
